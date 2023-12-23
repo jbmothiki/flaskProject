6f6d5d27d6d028a2f33e4ad8f6f691791a3a4db5
Jagadeesh B Mothiki <jb.mothiki@gmail.com>
2023-12-23 21:49:55 +0530

    Basic flask app

--- app.py
+++ app.py
@@ -4,8 +4,8 @@ app = Flask(__name__)
 
 
 @app.route('/')
-def hello_world():  # put application's code here
-    return 'Hello World!'
+def hello_world():
+    return 'Hello, from jbmothiki'
 
 
 if __name__ == '__main__':
